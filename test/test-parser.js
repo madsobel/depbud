d402bb687a44ea0377c3ee6d1a2b3388500a0f24
Mads Obel <madsobelcph@gmail.com>
2017-09-01 13:06:49 +0200

    Added a dummy test

---
+++ test/test-parser.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+describe('Parser', function() {
+    describe('foo', function() {
+        it('should return true', function() {
+            assert.equal(true, true);
+        });
+    });
+});
